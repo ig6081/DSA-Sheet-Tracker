b84b8d2c473160ebd5eb918fd4adb7e1ad4c5a76
Indrajeet Gupta <ig6081@srmist.edu.in>
2023-05-03 01:36:06 +0530

    done

--- src/components/Topic/Topic.js
+++ src/components/Topic/Topic.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect, useContext } from 'react';
 import BootstrapTable from 'react-bootstrap-table-next';
 import ToolkitProvider from 'react-bootstrap-table2-toolkit';
